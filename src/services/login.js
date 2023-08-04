f67ed06f5aa6dd41f8948c046e0ab1ebe4e62272
luoming <18618321192@163.com>
2023-08-04 18:17:51 +0800

    Mod

--- src/services/login.js
+++ src/services/login.js
@@ -1,6 +1,6 @@
-
-import { request } from '@/utils/request'
+import request from '@/utils/request'
 // 获取验证码
-export async function authInfo(param) {
-    return request('api2/WuWeiBlueCompanyPlace/LogoAndTitle', 'GET', param)
+export async function authInfo (param) {
+  // return request('api2/WuWeiBlueCompanyPlace/LogoAndTitle', 'GET', param)
+  return request.get('api2/WuWeiBlueCompanyPlace/LogoAndTitle', param)
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,194 +1,114 @@
-import axios from 'axios'
+// axios二次封装
+// yarn add axios-adapter-uniapp import axiosAdapterUniapp from 'axios-adapter-uniapp'
 import { getToken } from '@/utils/auth'
-import qs from 'qs'
-import { Toast } from 'vant'
-// 正在进行中的请求列表
-let reqList = []
+import axios from 'axios'
+// 小程序axios适配器
+import mpAdapter from 'axios-miniprogram-adapter'
+axios.defaults.adapter = mpAdapter
 
-const service = axios.create()
-// 跨域认证信息 header 名
-const xsrfHeaderName = 'token'
-const BASE_URL = JSON.parse(process.env.VITE_BASE_URL) 
+//根据环境变量获取api地址
+let baseURL = process.env.config[process.env.UNI_SCRIPT].VITE_BASE_API
+// let evnName = process.env.config[process.env.UNI_SCRIPT] 获取当前处于哪个开发环境
+console.log('baseURL:', baseURL, '++++++++++++++++++++++++')
 
-service.timeout = 120000
-service.withCredentials = true
-service.xsrfHeaderName = xsrfHeaderName
-service.xsrfCookieName = xsrfHeaderName
-
-const CancelToken = axios.CancelToken
-
-
-/**
- * axios请求
- * @param url 请求地址
- * @param method {METHOD} http method
- * @param params 请求参数
- * @returns {Promise<AxiosResponse<T>>}
- */
-async function request (url, method, params, contentType) {
-    debugger
-    switch (method) {
-    case 'GET':
-        return service.get( BASE_URL+ url, { params }, contentType)
-    case 'POST':
-        return service.post(BASE_URL + url, params, contentType)
-    case 'FORM':
-        console.log(url, params, contentType)
-        return service.post(url, params, contentType)
-    default:
-        return service.get(BASE_URL + url, { params }, contentType)
+class HttpRequest {
+  constructor () {
+    this.baseURL = baseURL // 从环境变量中获取api地址
+    this.timeout = 300000
+  }
+  mergeOptions (options) {
+    return {
+      baseURL,
+      timeout: 300000,
+      ...options
     }
-}
-function whitePath (path) {
-    let list = ['/ent/auth', '/login', '/info', '/ent/sign'] //不需要token的接口
-    let bol = false
-    for (let i = 0; i < list.length; i++) {
-        if (path.indexOf(list[i]) > -1) {
-            bol = true
-            break
-        }
-    }
-    return bol
-}
+  }
+  request (options) {
+    const instance = axios.create()
+    this.setInterceptors(instance)
+    const opts = this.mergeOptions(options)
+    return instance(opts)
+  }
+  get (url, data = {}, outHeaders = {}) {
+    // console.log(data, "data+++++++++++++");
 
-service.interceptors.request.use(config => {
-    if (config.method === 'get') {
-    // 如果是get请求，处理数组传值
-        config.paramsSerializer = function (params) {
-            return qs.stringify(params, { arrayFormat: 'repeat' })
-        }
-    }
-    const source = CancelToken.source()
-    // 全局添加cancelToken
-    config.cancelToken = source.token
-    //保存可能阻止的请求
-    stopRepeatRequest(reqList, config.url, source)
+    return this.request({
+      dataType: 'json',
+      method: 'get',
+      url,
+      params: { ...data }, // get参数可以直接展开
+      headers: {}
+    })
+  }
+  post (url, data = {}, outHeaders = {}) {
+    // 请求体中 {}
+    return this.request({
+      method: 'post',
+      url,
+      data, // post要求必须传入data属性
+      headers: {}
+    })
+  }
 
-    //token没有值
-    if (xsrfHeaderName && !getToken()) {
-    //无token认证信息，进行授权或者跳转登录
-        if (!whitePath(config.url)) {
-            //
-        }
-    } else {
-    //设置token
-        console.log(getToken())
-        if (getToken() && !whitePath(config.url)) {
-            config.headers = {
-                'Cookie': getToken(),
-            }
-        } else {
-            config.headers = {
-                'Cookie': '',
-            }
-        }
-    }
-    return config
-}, error => { //请求错误处理
-    Promise.reject(error)
-})
-
-service.interceptors.response.use((res) => {
-    //console.log('响应信息=>', res)
-    if (res.data.code === 401) {
-    // 取消其他正在进行的请求
-        for (let item of reqList) {
-            item.source.cancel()
-        }
-        //删除
-        removeAuthorization()
-        //Toast('登录信息过期，请重新登录')
-        uni.clearStorageSync()
-        // uni.clearStorage()
-        // //暂存当前地址用于跳转
-        // const path = location.hash.replace('#', '')
-        // if (path !== '/')
-        //     uni.setStorageSync('defUrl', path)
-
-        uni.reLaunch({
-            url: '/pages/login/index',
+  // 设置拦截器
+  setInterceptors (instance) {
+    // 请求拦截器
+    instance.interceptors.request.use(config => {
+      const noLoading = config.headers['NO-LOADING']
+      // 是否需要设置 token
+      const isToken = config.headers['ISTOKEN'] || false
+      if (getToken() && isToken) {
+        config.header['Cookie'] = `${getToken()}`
+      }
+      if (!noLoading) {
+        uni.showLoading({
+          title: '加载中...'
         })
-        //清空所有阻止的请求
-        reqList = []
-
-        return Promise.reject(res)
-    } else if (res.data.code !== 200) {
-        Toast({
-            message: res.data.msg, duration: 2500,
-        })
-        return Promise.reject(res)
-    }
-
-    //删除阻止强求source
-    allowRequest(reqList, res.config.url)
-
-    //返回正常接口
-    return res.data
-}, (error) => {
-    if (axios.isCancel(error)) {
-        return new Promise(() => { })
-    }
-    else {
-    // ...请求失败后的后续操作
-        if (error.code == 'ECONNABORTED' && error.message.indexOf('timeout')) {
-            Toast('请求超时，请检查网网络连接')
+      }
+      config.headers = {
+        ...config.headers
+      }
+      //console.log('config',config)
+      return config
+    })
+    // 响应拦截器
+    instance.interceptors.response.use(
+      res => {
+        const noLoading = res.config.headers['NO-LOADING']
+        if (!noLoading) {
+          uni.hideLoading()
         }
-        else {
-            Toast('服务错误，请联系管理员')
+        let { data } = res
+        // console.log("请求获取data", data)
+        if (data) {
+          if (data.code === 200) {
+            //console.log('data=============', data)
+            return Promise.resolve(data)
+          } else {
+            uni.showToast({
+              title: '111'
+            })
+            // showConfirm({
+            //   content: data.msg,
+            //   showCancel: false
+            // }).then(res => {
+            //   /*               if (res.confirm) {
+            //     store.dispatch("LogOut").then((res) => {
+            //       uni.reLaunch({ url: "/pages/login" });
+            //     });
+            //   } */
+            // })
+            return Promise.resolve(data)
+          }
         }
-    }
-
-    return Promise.reject(error)
-})
-//设置token认证信息
-function setAuthorization (auth) {
-    Cookie.set(xsrfHeaderName, auth.token, { expires: auth.expireAt })
-}
-/**
- * 获取Token
- */
-function getAuthorization () {
-    return Cookie.get(xsrfHeaderName)
-}
-/**
- * 移出认证信息
- */
-function removeAuthorization () {
-    Cookie.remove(xsrfHeaderName)
+      },
+      err => {
+        console.log('axios报错', err)
+        uni.hideLoading()
+        return Promise.reject(err)
+      }
+    )
+  }
 }
 
-
-/* 阻止重复请求
-* @param {array} reqList - 请求缓存列表
-* @param {string} url - 当前请求地址
-* @param {function} cancel - 请求中断函数
-*/
-const stopRepeatRequest = function (reqList, url, cancel) {
-    for (let i = 0; i < reqList.length; i++) {
-        if (reqList[i].url === url) {
-            return
-        }
-    }
-    reqList.push({ url, source: cancel })
-}
-
-/**
- * 允许某个请求可以继续进行
- * @param {array} reqList 全部请求列表
- * @param {string} url 请求地址
- */
-const allowRequest = function (reqList, url) {
-    for (let i = 0; i < reqList.length; i++) {
-        if (reqList[i].url === url) {
-            reqList.splice(i, 1)
-            break
-        }
-    }
-}
-
-export {
-    request,
-    setAuthorization,
-    getAuthorization,
-    removeAuthorization,
-}
+export default new HttpRequest()
